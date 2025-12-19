aec34810be8cde578e49c49b69daff89a0af5221
Chidiebere <chidiebereuzomahumble@gmail.com>
2025-12-19 03:54:39 +0100

    Wrapped VerifyEmailComponent with Suspense boundary

--- app/verify-email/page.tsx
+++ app/verify-email/page.tsx
@@ -1,5 +1,17 @@
+import { Suspense } from 'react'
 import VerifyEmailComponent from '@/components/auth/VerifyEmailComponent'
+import { Spinner, Center } from '@chakra-ui/react'
 
 export default function VerifyEmailPage() {
-  return <VerifyEmailComponent />
+  return (
+    <Suspense
+      fallback={
+        <Center minH="100vh">
+          <Spinner size="xl" />
+        </Center>
+      }
+    >
+      <VerifyEmailComponent />
+    </Suspense>
+  )
 }
